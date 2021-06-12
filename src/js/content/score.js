5b5fa62fe05570b07773a94c84c739d44b1a98de
nicross <nicross@users.noreply.github.com>
2021-06-12 02:23:40 -0500

    Add some point bonuses

--- src/js/content/score.js
+++ src/js/content/score.js
@@ -14,6 +14,11 @@ content.score = (() => {
   }
 })()
 
+engine.ready(() => {
+  content.train.on('add', () => content.score.increment(25))
+  content.train.on('remove', (props) => content.score.increment(-props.length))
+})
+
 engine.loop.on('frame', ({delta, paused}) => {
   if (paused) {
     return
