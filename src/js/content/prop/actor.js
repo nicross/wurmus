04222cc1297fd09212200f52e2b0e33e56331708
nicross <nicross@users.noreply.github.com>
2021-06-11 20:07:00 -0500

    Define basic train movement

--- src/js/content/prop/actor.js
+++ src/js/content/prop/actor.js
@@ -1,5 +1,6 @@
 content.prop.actor = engine.prop.base.invent({
   name: 'actor',
+  radius: 0.5,
   onConstruct: function () {
     this.footstepper = content.utility.footstepper.create({
       parameters: {
@@ -11,14 +12,59 @@ content.prop.actor = engine.prop.base.invent({
   onDestroy: function () {
 
   },
+  onTrainAdd: function () {
+    this.isTrain = true
+    return this
+  },
+  onTrainRemove: function () {
+    this.isTrain = false
+    return this
+  },
   onUpdate: function () {
-    this.move()
+    if (this.isTrain) {
+      this.moveTrain()
+    } else {
+      this.moveTag()
+    }
 
     this.footstepper.update({
       position: this.vector(),
     })
   },
-  move: function () {
+  calculateStoppingDistance: function () {
+    const deceleration = 8,
+      delta = engine.loop.delta(),
+      minimum = this.radius * 2,
+      velocity = this.velocity.distance()
 
+    return minimum + (velocity * delta) + ((velocity ** 2) / (2 * deceleration))
+  },
+  moveTag: function () {
+    // Roll whether to update based on difficulty
+    // Calculate desired position
+      // If running away, or closest is player, opposite direction from player
+      // Otherwise closest train actor
+    // Accelerate toward it
+
+    return this
+  },
+  moveTrain: function () {
+    const index = content.train.indexOf(this)
+
+    const ahead = index > 0
+      ? content.train.get(index - 1).vector()
+      : engine.position.getVector()
+
+    const velocity = ahead.distance(this) > this.calculateStoppingDistance()
+      ? ahead.subtract(this).normalize().scale(4) // max velocity
+      : engine.utility.vector3d.create() // zero
+
+    const rate = this.velocity.distance() > velocity.distance()
+      ? 4 // accelerate
+      : 8 // decelerate
+
+    this.velocity = content.utility.accelerate.vector(this.velocity, velocity, rate)
+
+    return this
   },
 })
--- src/js/content/train.js
+++ src/js/content/train.js
@@ -4,22 +4,34 @@ content.train = (() => {
 
   return engine.utility.pubsub.decorate({
     add: function (prop) {
-      if (!this.has(prop)) {
-        train.unshift(prop)
-        pubsub.emit('add', prop)
+      if (this.has(prop)) {
+        return this
       }
+
+      prop.onTrainAdd()
+      train.unshift(prop)
+      pubsub.emit('add', prop)
 
       return this
     },
     get: (index) => train[index],
     has: (prop) => train.includes(prop),
+    indexOf: (prop) => train.indexOf(prop),
     length: () => train.length,
     remove: function () {
       const index = train.indexOf(prop)
 
-      if (index > -1) {
-        pubsub.emit('remove', train.splice(index))
+      if (index == -1) {
+        return this
       }
+
+      const props = train.splice(index)
+
+      for (const prop of props) {
+        prop.onTrainRemove()
+      }
+
+      pubsub.emit('remove', props)
 
       return this
     },
@@ -29,3 +41,5 @@ content.train = (() => {
     },
   }, pubsub)
 })()
+
+engine.state.on('reset', () => content.train.reset())
