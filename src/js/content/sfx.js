0af3888e37fb31676dfd487f149e520a1088ca29
nicross <nicross@users.noreply.github.com>
2021-06-11 19:07:49 -0500

    Implement basic footstep sound

--- src/js/content/sfx.js
+++ src/js/content/sfx.js
@@ -4,7 +4,24 @@ content.sfx.bus = engine.audio.mixer.createBus()
 content.sfx.bus.gain.value = engine.utility.fromDb(0)
 
 content.sfx.footstep = function ({
+  color = 0.5,
   destination = content.sfx.bus,
+  frequency = 440,
+  velocity = 1,
 } = {}) {
-  console.log('footstep')
+  const synth = engine.audio.synth.createSimple({
+    frequency,
+    type: 'square',
+  }).filtered({
+    frequency: frequency * color * engine.utility.lerp(0.5, 2, velocity),
+  }).connect(destination)
+
+  const duration = 0.5,
+    gain = engine.utility.fromDb(engine.utility.lerp(-12, -6, velocity)),
+    now = engine.audio.time()
+
+  synth.param.gain.exponentialRampToValueAtTime(gain, now + 1/32)
+  synth.param.gain.exponentialRampToValueAtTime(engine.const.zeroGain, now + duration)
+
+  synth.stop(now + duration)
 }
