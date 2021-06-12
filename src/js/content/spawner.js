5d06e82566c507739cc99d9fe99ac0ab012d74a3
nicross <nicross@users.noreply.github.com>
2021-06-11 20:07:34 -0500

    Spawn train at start of game

---
+++ src/js/content/spawner.js
@@ -0,0 +1,45 @@
+content.spawner = (() => {
+  function generateOptions() {
+    return {}
+  }
+
+  function initializeGame() {
+    const count = 1,
+      distance = content.prop.actor.radius * 2,
+      position = engine.position.getVector()
+
+    for (let i = 0; i < count; i += 1) {
+      const prop = engine.props.create(content.prop.actor, {
+        ...generateOptions(),
+        ...position.add({x: -i * distance}),
+      })
+
+      content.train.add(prop)
+    }
+  }
+
+  return {
+    import: function () {
+      initializeGame()
+      return this
+    },
+    reset: function () {
+      return this
+    },
+    update: function () {
+      // Spawn waves
+      return this
+    },
+  }
+})()
+
+engine.loop.on('frame', ({delta, paused}) => {
+  if (paused) {
+    return
+  }
+
+  content.score.increment(delta)
+})
+
+engine.state.on('import', () => content.spawner.import())
+engine.state.on('reset', () => content.spawner.reset())
