06631de1162d128b9b9727bfa8c80d6abdf201dd
nicross <nicross@users.noreply.github.com>
2021-06-27 19:25:06 -0500

    Add title theme lick

--- docs/CHANGELOG.md
+++ docs/CHANGELOG.md
@@ -2,6 +2,7 @@
 ## v1.1.0
 - Added animations to screens and transitions between them
 - Added directional audio cue to indicate new enemy spawns
+- Added title theme song
 - Added visual particle effects to collisions
 - Added visual invincibility indicator for actors
 - Fixed invincibility not applying to train members
--- src/js/content/music.js
+++ src/js/content/music.js
@@ -1,27 +1,119 @@
 content.music = (() => {
-  const bus = engine.audio.mixer.createBus()
+  const bus = engine.audio.mixer.createBus(),
+    reverb = engine.audio.mixer.send.reverb.create()
 
-  let leftBinaural,
+  let centerLfo,
+    centerPhaser,
+    centerSynth,
+    leftBinaural,
     leftSynth,
+    kickSynth,
     rightBinaural,
     rightSynth,
     subSynth
 
   bus.gain.value = engine.const.zeroGain
+  reverb.from(bus)
 
   function createSynths() {
+    centerSynth = engine.audio.synth.createSimple({
+      detune: engine.utility.random.float(-5, 5),
+      frequency: engine.utility.midiToFrequency(68),
+      gain: engine.utility.fromDb(-6),
+      type: 'sawtooth',
+    }).filtered({
+      frequency: engine.utility.midiToFrequency(68),
+    }).chainAssign('phaser', engine.audio.effect.createPhaser({
+      depth: 0.005,
+      dry: 0,
+      feedback: 0.825,
+      frequency: 1/8,
+      wet: 1,
+    })).connect(bus)
+
+    centerLfo = engine.audio.synth.createLfo({
+      depth: -100,
+      frequency: 1/12,
+    }).shaped(
+      engine.audio.shape.distort()
+    ).connect(centerSynth.param.detune)
+
+    kickSynth = engine.audio.synth.createAm({
+      carrierFrequency: engine.utility.midiToFrequency(24),
+      carrierGain: 0,
+      carrierType: 'sawtooth',
+      gain: engine.utility.fromDb(-3),
+      modDepth: 1,
+      modFrequency: 1,
+      modType: 'sawtooth',
+    }).filtered({
+      frequency: engine.utility.midiToFrequency(24) * 4,
+    }).connect(bus)
+
+    leftSynth = engine.audio.synth.createAm({
+      carrierDetune: engine.utility.random.float(-5, 5),
+      carrierFrequency: engine.utility.midiToFrequency(52),
+      carrierGain: 3/4,
+      carrierType: 'sawtooth',
+      gain: engine.utility.fromDb(-3),
+      modDepth: 1/4,
+      modFrequency: 3,
+      modType: 'triangle',
+    }).filtered({
+      frequency: engine.utility.midiToFrequency(60) * 2,
+    })
+
     leftBinaural = engine.audio.binaural.create({
       x: 0.5,
       y: 0.5,
+    }).from(leftSynth).to(bus)
+
+    rightSynth = engine.audio.synth.createAm({
+      carrierDetune: engine.utility.random.float(-5, 5),
+      carrierFrequency: engine.utility.midiToFrequency(60),
+      carrierGain: 3/4,
+      carrierType: 'sawtooth',
+      gain: engine.utility.fromDb(-3),
+      modDepth: 1/4,
+      modFrequency: 4,
+      modType: 'triangle',
+    }).filtered({
+      frequency: engine.utility.midiToFrequency(60) * 2,
     })
 
     rightBinaural = engine.audio.binaural.create({
       x: 0.5,
       y: -0.5,
-    })
+    }).from(rightSynth).to(bus)
+
+    subSynth = engine.audio.synth.createSimple({
+      frequency: engine.utility.midiToFrequency(24),
+      gain: 1,
+      type: 'sine',
+    }).connect(bus)
   }
 
   function destroySynths() {
+    if (centerLfo) {
+      centerLfo.stop()
+      centerLfo = undefined
+    }
+
+    if (centerPhaser) {
+      centerPhaser.stop()
+      centerPhaser = undefined
+    }
+
+    if (centerSynth) {
+      centerSynth.stop()
+      centerSynth = undefined
+    }
+
+    if (kickSynth) {
+      kickSynth.stop()
+      kickSynth = undefined
+    }
+
     if (leftBinaural) {
       leftBinaural.destroy()
       leftBinaural = undefined
@@ -50,10 +142,10 @@ content.music = (() => {
 
   return {
     start: function () {
-      const attack = 2
+      const attack = 4
 
       createSynths()
-      engine.audio.ramp.linear(bus.gain, engine.utility.fromDb(-18), attack)
+      engine.audio.ramp.linear(bus.gain, engine.utility.fromDb(-15), attack)
 
       return this
     },
