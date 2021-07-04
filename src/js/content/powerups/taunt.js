69e679342c7f0abb6f5d93cdcf960df1a3fcd637
nicross <nicross@users.noreply.github.com>
2021-07-03 19:57:30 -0500

    Tweak taunt sfx to be more distinct from invincibility

--- src/js/content/powerups/taunt.js
+++ src/js/content/powerups/taunt.js
@@ -20,15 +20,15 @@ content.powerups.taunt = content.powerups.register({
       now = engine.audio.time()
 
     const synth = engine.audio.synth.createMod({
-      amodDepth: 1/3,
+      amodDepth: 1/2,
       amodFrequency: frequency * 2,
       amodType: 'square',
       carrierDetune: 0,
       carrierFrequency: frequency,
-      carrierGain: 2/3,
+      carrierGain: 1/2,
       carrierType: 'triangle',
       fmodDetune: 0,
-      fmodDepth: frequency,
+      fmodDepth: -frequency,
       fmodFrequency: 8,
       fmodType: 'sawtooth',
     }).filtered({
@@ -36,7 +36,7 @@ content.powerups.taunt = content.powerups.register({
       frequency,
     }).connect(content.powerups.bus())
 
-    synth.filter.detune.linearRampToValueAtTime(2400, now + this.duration)
+    synth.filter.detune.linearRampToValueAtTime(3600, now + this.duration)
     synth.param.detune.linearRampToValueAtTime(-1200, now + this.duration)
     synth.param.amod.detune.linearRampToValueAtTime(2400, now + this.duration)
     synth.param.fmod.frequency.linearRampToValueAtTime(1/8, now + this.duration)
